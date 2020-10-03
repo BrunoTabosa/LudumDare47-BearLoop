91b0967e93999e494de4ff823b42186a99194aaa
Bruno Tabosa <brunotabosasnt@gmail.com>
2020-10-03 10:57:22 -0300

    Initial commit. Basic movement basic camera controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+public class GameController : MonoBehaviour
+{
+	public Transform RespawnPoint;
+	public Player playerPrefab;
+    public CameraController cameraController;
+
+
+
+	public Player currentPlayer;
+
+
+    private void Start()
+    {
+        if(currentPlayer == null)
+            currentPlayer = Instantiate(playerPrefab, RespawnPoint.position, Quaternion.identity);
+
+        cameraController.Follow(currentPlayer);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Respawn());
+        }
+    }
+    public IEnumerator Respawn()
+    {
+        currentPlayer?.Die();
+        yield return new WaitForSeconds(1.5f);
+        currentPlayer = Instantiate(playerPrefab, RespawnPoint.position, Quaternion.identity);
+        cameraController.Follow(currentPlayer);
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed;
+
+    [SerializeField]
+    private CharacterController characterController;
+
+
+    private float horizontal, vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    
+
+    void HandleInput()
+    {
+        horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        vertical = Input.GetAxis("Vertical") * movementSpeed;
+
+        characterController.SimpleMove(new Vector3(horizontal, 0, vertical));
+    }
+
+    public void Die()
+    {
+        characterController.enabled = false;
+        this.enabled = false;
+    }
+}
