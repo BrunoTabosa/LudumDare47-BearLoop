28c5ffb6195c5ff02aa537e572b08bd7d28074b6
Bruno Tabosa <brunotabosasnt@gmail.com>
2020-10-04 18:02:53 -0300

    Adding animation to door

---
+++ Assets/Scripts/Interactables/AnimatedInteractable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedInteractable : MonoBehaviour, IInteractable
+{
+    public Animation animation;
+    public AnimationClip InteractClip;
+    public AnimationClip CancelInteractClip;
+
+    void Start()
+    {
+        //animation.AddClip(InteractClip, InteractClip.name);
+        //animation.AddClip(CancelInteractClip, CancelInteractClip.name);
+    }
+
+    public void CancelInteraction()
+    {
+        if(CancelInteractClip)
+        {
+            animation.clip = CancelInteractClip;
+            animation.Play();
+        }
+    }
+
+    public void Interact()
+    {
+        if (InteractClip)
+        {
+            animation.clip = InteractClip;
+            animation.Play();
+        }
+    }
+}
