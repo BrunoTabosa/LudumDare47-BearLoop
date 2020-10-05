266fb72767aca6a5ef585cb59b9de4c6b56fe734
henrique <henriquelima1408@hotmail.com>
2020-10-05 18:13:44 -0300

    -Green / Red Light

---
+++ Assets/Scripts/InteractibleLight.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractibleLight : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private UnityEvent _onInteract;
+    [SerializeField]
+    private UnityEvent _onCancelInteract;
+
+    public void CancelInteraction()
+    {
+        _onCancelInteract?.Invoke();
+    }
+
+    public void Interact()
+    {
+        _onInteract?.Invoke();
+    }
+}
